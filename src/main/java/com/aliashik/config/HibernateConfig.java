d3232d3eef34d6a6192dbd6468505800edebdb82
r.ali.ashik <ramjan.ali.ashik@gmail.com>
2018-04-03 15:45:58 +0600

    new change

--- src/main/java/com/aliashik/config/HibernateConfig.java
+++ src/main/java/com/aliashik/config/HibernateConfig.java
@@ -49,12 +49,10 @@ public class HibernateConfig {
     @Bean
     public Properties hibernateProperties(){
         Properties properties = new Properties();
-
         properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
         properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
         properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.automaticschema"));
-
         return properties;
     }
 
